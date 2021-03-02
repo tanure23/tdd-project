e288a7e21a135c03c704ba42ed9252b1b1228185
Breno Tanure <brenotpr@gmail.com>
2021-03-01 23:58:59 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 3

--- lists/views.py
+++ lists/views.py
@@ -7,9 +7,11 @@ def home_page(request):
         Item.objects.create(text=new_item_text)
         return redirect('/lists/the-only-list-in-the-world/')
 
+    return render(request, 'home.html')
+
     items = Item.objects.all()
     return render(request, 'home.html', {'items': items})
 
 def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
